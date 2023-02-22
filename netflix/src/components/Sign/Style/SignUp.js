7e41bb6891c8b8d4e70ab96fe22f72e9d7acc9c1
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-02-22 22:29:07 +0900

    ADD::sign up header style

---
+++ netflix/src/components/Sign/Style/SignUp.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const container = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
+
+export const Header = styled.nav`
+    width: 100%;
+    height: 5rem;
+    /* background-color: #e1e1e1; */
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const Logo = styled.h1`
+    font-size: 36px;
+    margin-left: 50px;
+`
+
+export const Btn = styled.button`
+    width: 100px;
+    height: 50px;
+    background: #e50914;
+    font-weight: 400;
+    color: white;
+    font-size: 24px;
+    border: none;
+    border-radius: 3px;
+    margin-right: 50px;
+`
---
+++ netflix/src/pages/signUp.js
@@ -0,0 +1,11 @@
+import * as style from "../components/Sign/Style/SignUp"
+export default function SignUp() {
+    return (
+        <style.container>
+            <style.Header>
+                <style.Logo>NETFLIX</style.Logo>
+                <style.Btn>로그인</style.Btn>
+            </style.Header>
+        </style.container>
+    )
+}
