ab219fac8090de0d04d9c095c51b3a51aaebffca
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-02-28 21:19:55 +0900

    signup header

--- netflix/src/components/Header/style/style.js
+++ netflix/src/components/Header/style/style.js
@@ -2,11 +2,12 @@ import styled from "styled-components";
 
 export const container = styled.nav`
     width: 100%;
-    height: 70px;
+    height: 80px;
     border-bottom: 1px solid #e6e6e6;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-evenly;
+    gap: 1200px;
 `
 
 export const login = styled.span`
