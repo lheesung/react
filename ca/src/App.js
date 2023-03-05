e705109c24e66e9a4d8a4934d69f562760499c88
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-03-05 22:31:44 +0900

    useState()

--- ca/src/App.js
+++ ca/src/App.js
@@ -1,7 +1,20 @@
+import "./App.css"
+import { useState } from 'react';
+
 function App() {
+  let post = 'Hello World!';
+  let [postName,setPost] = useState('Arsenal'); // 자주 변경될 것 같은 돔은 useState 로 관리.
+  let [logo, setLogo] = useState('React Blog');
   return (
-    <>
-    </>
+    <div className="App">
+      <div className="nav">
+        <h1>{logo}</h1>
+      </div>
+      <div className="list">
+        <h1>{postName}</h1>
+        <p>date</p>
+      </div>
+    </div>
   );
 }
 
