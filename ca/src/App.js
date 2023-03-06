0500da767a7420e658993e769d08ae3021baa3cc
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-03-06 17:35:20 +0900

    map()

--- ca/src/App.js
+++ ca/src/App.js
@@ -2,47 +2,41 @@ import "./App.css"
 import { useState } from 'react';
 
 function App() {
-  let [postName,setPost] = useState(['Hello', 'World', '!!!!!']); // 자주 변경될 것 같은 돔은 useState 로 관리.
-  let [like,setLikes] = useState(0);
+  let [postName, setPost] = useState(['Hello', 'World', '!!!!!']); // 자주 변경될 것 같은 돔은 useState 로 관리.
+  let [like, setLikes] = useState([0, 0, 0]);
   let [logo, setLogo] = useState('React Blog');
+  let [modal, setModal] = useState(false);
+
   return (
     <div className="App">
       <div className="nav">
         <h1>{logo}</h1>
       </div>
-      
-      <button onClick={() => {
-        let copy = [...postName];
-        copy[0] = 'Hola';
-        setPost(copy);
-        }}>change state</button>
-
-      <div className="list">
-        <h1>{postName[0]} <span onClick={() => {setLikes(like++)}}>👍</span> {like} </h1>
-        <p>date</p>
-      </div>
-
-      <div className="list">
-        <h1>{postName[1]}</h1>
-        <p>date</p>
-      </div>
-
-      <div className="list">
-        <h1>{postName[2]}</h1>
-        <p>date</p>
-      </div>
-      <Modal />
+      {postName.map(function (a, i) {
+        return (
+          <div className="list">
+            <h1  onClick={() => { setModal(!modal) }}>{postName[i]}</h1>
+            <span onClick={() => {
+              let copy = [...like];
+              copy[i] += 1;
+              setLikes(copy);
+            }}>👍 {like[i]}</span>
+            <p>date</p>
+          </div>
+        )
+      })}
+      {modal == true ? <Modal /> : null}
     </div>
   );
 }
 
-function Modal(){
-  return(
+function Modal() {
+  return (
     <div className="modal">
-        <h4>제목</h4>
-        <p>내용</p>
-        <p>날짜</p>
-      </div>
+      <h4>제목</h4>
+      <p>내용</p>
+      <p>날짜</p>
+    </div>
   )
 }
 
